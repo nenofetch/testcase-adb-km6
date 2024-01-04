18e330111c30679cb451ffa39731984a304f2e88
Neno Arisma <nenoarisma@gmail.com>
2024-01-05 02:54:38 +0700

    feat: adding route products

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// Import library
 import Express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import multer from "multer";
+import cors from "cors";
+import router from "./src/routes/routes";
+
+// declaration variable
 const port = process.env.PORT || 3000;
 const app = Express();
+const formData = multer();
+const prefix = "/api/v1";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+app.use(morgan("combined"));
+// for handling multipart
+app.use(formData.array());
 
-
+app.use(`${prefix}/`, router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,18 @@
 import Express from "express";
+import {
+  getAllProducts,
+  createProduct,
+  detailProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/product.controller";
 
 const router = Express.Router();
+
+router.get("products", getAllProducts);
+router.post("products", createProduct);
+router.get("products/:id", updateProduct);
+router.delete("products/:id", deleteProduct);
+router.get("products/:id", detailProduct);
+
+export default router;
