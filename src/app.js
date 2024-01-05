22ffa3445223b8d95529cfc1d9bddd530cf1a0df
Neno Arisma <nenoarisma@gmail.com>
2024-01-05 14:53:01 +0700

    chore: update app.js and package.json

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 // Import library
-import Express from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import multer from "multer";
-import cors from "cors";
-import router from "./src/routes/routes";
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const multer = require("multer");
+const cors = require("cors");
+const router = require("./routes/routes");
 
 // declaration variable
 const port = process.env.PORT || 3000;
-const app = Express();
+const app = express();
 const formData = multer();
-const prefix = "/api/v1";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,7 +18,7 @@ app.use(morgan("combined"));
 // for handling multipart
 app.use(formData.array());
 
-app.use(`${prefix}/`, router);
+app.use(`/api/v1/products`, router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
